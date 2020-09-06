4c93a88c4440b0b6ff39be145368741cc575f2c0
smaccary <smaccary@student.42.fr>
2020-09-06 21:47:24 +0300

    tab

--- pisciner_check.sh
+++ pisciner_check.sh
@@ -2,7 +2,7 @@
 CAMPUS=$(curl -s -b cookies.txt "https://profile.intra.42.fr/users/$1/coalitions?cursus=42cursus" | grep campus_id)
 if [ -z "$CAMPUS" ];
 then
-	echo "$1 : [FAIL]" >&2
+	echo "$1 :	[FAIL]" >&2
 else
-	echo "$1 : [SUCCESS]"
+	echo "$1 :	[SUCCESS]"
 fi
