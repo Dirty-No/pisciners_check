26a194dfeb27e130fa072f66a106b1587d8f8020
Noëlle <54525684+Dirty-No@users.noreply.github.com>
2020-09-07 02:19:07 +0300

    Update sign_in.sh

--- sign_in.sh
+++ sign_in.sh
@@ -43,26 +43,3 @@ if [ -z  "$CHECK" ];
 fi
 
 printf "\e[0m\n"
-exit
-
-curl 'https://signin.intra.42.fr/users/sign_in' \
-  -H 'Connection: keep-alive' \
-  -H 'Cache-Control: max-age=0' \
-  -H 'Origin: https://signin.intra.42.fr' \
-  -H 'Upgrade-Insecure-Requests: 1' \
-  -H 'DNT: 1' \
-  -H 'Content-Type: application/x-www-form-urlencoded' \
-  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36' \
-  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
-  -H 'Sec-Fetch-Site: same-origin' \
-  -H 'Sec-Fetch-Mode: navigate' \
-  -H 'Sec-Fetch-User: ?1' \
-  -H 'Sec-Fetch-Dest: document' \
-  -H 'Referer: https://signin.intra.42.fr/users/sign_in' \
-  -H 'Accept-Language: fr' \
-  -H 'Cookie: pro.id=bnVsbA%3D%3D--af7657a8234901541e1115b23c7a73d93a7191fd; user.id=bnVsbA%3D%3D--af7657a8234901541e1115b23c7a73d93a7191fd; _intra_42_session_production=539a4fb63b2bb497cf8f714dd05d0616' \
-  --data-raw 'utf8=%E2%9C%93&authenticity_token=pYm%2B%2F0XwpoB5CYYuTwaSCdPgx4DDs%2FlhrLVc%2Fq99Floyzc1bXsZz5%2BnG8WVvbD48UIWNKcbbkFaFxHPWsU2Rfg%3D%3D&user%5Blogin%5D=smaccary&user%5Bpassword%5D=motdepasse%40&commit=Sign+in' \
-  --compressed
-
-pYm+/0XwpoB5CYYuTwaSCdPgx4DDs/lhrLVc/q99Floyzc1bXsZz5+nG8WVvbD48UIWNKcbbkFaFxHPWsU2Rfg==
-pYm%2B%2F0XwpoB5CYYuTwaSCdPgx4DDs%2FlhrLVc%2Fq99Floyzc1bXsZz5%2BnG8WVvbD48UIWNKcbbkFaFxHPWsU2Rfg%3D%3D
