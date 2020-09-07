86373abb87034aca67ee5f068fa2adf1f7eb5b7d
smaccary <smaccary@student.42.fr>
2020-09-07 03:03:09 +0300

    added stuff

--- check_list.sh
+++ check_list.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-cat "$1" | xargs -L1 ./pisciner_check.sh > SUCCESS 2> FAIL &
+if [ $# -ne 1 ];
+then
+    echo "Invalid arguments"
+    echo "Usage : ./check_list.sh login_list.txt"
+    exit
+fi
+
+cat "$1" | xargs -L1 ./pisciner_check.sh > SUCCESS.txt 2> FAIL.txt &
--- pisciner_check.sh
+++ pisciner_check.sh
@@ -2,7 +2,7 @@
 CAMPUS=$(curl -s -b cookies.txt "https://profile.intra.42.fr/users/$1/coalitions?cursus=42cursus" | grep campus_id)
 if [ -z "$CAMPUS" ];
 then
-	echo "$1 :	[FAIL]" >&2
+	printf "%-10s :	[FAIL]\n" "$1" >&2
 else
-	echo "$1 :	[SUCCESS]"
+	printf "%-10s :	[SUCCESS]\n" "$1"
 fi 
