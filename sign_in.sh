1be51d49f8714af97b38835ca9002c7dee110eb8
Smaccary <smaccary@student.42.fr>
2020-09-07 15:08:40 +0200

    add stuff

--- sign_in.sh
+++ sign_in.sh
@@ -2,7 +2,7 @@
 
 convert_utf8()
 {
-	echo -n "$1"	\
+	echo "$1"	\
 	|	sed 's/%/%25/g'		|	sed 's/\&/%26/g'	|	sed 's/!/%21/g'	\
 	|	sed 's/"/%22/g'		|	sed 's/#/%23/g'		|	sed 's/\$/%24/g'	|	sed 's/(/%28/g'		\
 	|	sed 's/)/%29/g'		|	sed 's/+/%2B/g'		|	sed 's/,/%2C/g'		| 	sed 's/\//%2F/g'	\
@@ -21,20 +21,21 @@ read PASSWORD
 stty echo
 printf "\n"
 
-echo -n "" > cookies.txt
+echo "" > cookies.txt
 
 TOKEN=$(curl -s -c cookies.txt -b cookies.txt 'https://signin.intra.42.fr/users/sign_in' --compressed)
-TOKEN=$(echo -n "$TOKEN"	| sed '/<meta name="csrf-token" content="/!d;s//&\n/;s/.*\n//;:a;/"/bb;$!{n;ba};:b;s//\n&/;P;D')
+TOKEN=$(echo "$TOKEN" |  grep csrf-token | sed 's/<meta name="csrf-token" content="//g' | sed 's/" \/>//g')
 TOKEN=$(convert_utf8 "$TOKEN")
+
 
 PASSWORD=$(convert_utf8 "$PASSWORD")
 
 curl -s -c cookies.txt -b cookies.txt 'https://signin.intra.42.fr/users/sign_in' \
 --data-raw "utf8=%E2%9C%93&authenticity_token=$TOKEN&user%5Blogin%5D=$LOGIN&user%5Bpassword%5D=$PASSWORD&commit=Sign+in" \
---compressed > /dev/null
+--compressed > response.html
 
 CHECK="$(curl -s -b cookies.txt https://profile.intra.42.fr/)"
-CHECK=$(echo -n "$CHECK" | grep "Intra Profile Home")
+CHECK=$(echo "$CHECK" | grep "Intra Profile Home")
 
 if [ -z  "$CHECK" ];
 	then printf "\e[31mLOGIN : [FAILED]"
